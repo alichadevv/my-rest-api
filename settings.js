7b54dd5291983a460fa67627677d17ceb1fa9c1c
メイチャ <alichaicha248@gmail.com>
2025-04-08 17:04:54 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Xydlan'// yourname
+global.MONGO_DB_URI = "mongodb+srv://MeichaIcha:DqGGQb8aYWlSWeNe@cluster0.obid2pl.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "alichaicha248@gmail.com" //email
+global.email_password = "DqGGQb8aYWlSWeNe" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
